0de047eec43e40e1864f7d84d17b4966886e343a
Evan Chernicky <echernicky@gmail.com>
2022-04-12 15:48:23 -0400

    commit tree cf231b391b7b34e5bbd56fc608c0416e6800dc60

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
 
   def index
+    if params[:user_id]
+      user = User.find_by(id: params[:id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
